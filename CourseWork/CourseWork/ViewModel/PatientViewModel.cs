b0a93e22d822430475850d877e5493ec5ccab3b8
Valeria Makarevich <valeriamakarevich@outlook.com>
2022-11-08 22:09:30 +0300

    дополнила проект

--- CourseWork/CourseWork/ViewModel/PatientViewModel.cs
+++ CourseWork/CourseWork/ViewModel/PatientViewModel.cs
@@ -6,5 +6,20 @@ namespace CourseWork.ViewModel
 {
     public partial class PatientViewModel : BaseViewModel
     {
+
+        public ObservableCollection<PatientModel> Patient { get; set; } = new();
+        [RelayCommand]
+        void StartDemo()
+        {
+            if (IsBusy)
+            {
+                return;
+            }
+            var patient = new PatientModel();
+            patient.PatientID = "10";
+            patient.Name = "Nikita";
+            patient.Surname = "Laboda";
+            //Patients.Add(patient);
+        }
     }
 }
